b1b5ef14f20cbfc91b11ef90a701a1fab7a10e4c
TheodoraPetra <73179930+TheodoraPetra@users.noreply.github.com>
2024-11-11 15:14:10 +0100

    Injected more weather data

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ let apiKey = "064bdaa5f4b41a48e6cta92391004o0f";
 
 function displayTemperature(response) {
   let cityTemperature = document.querySelector(".weather-value");
+  let condition = document.querySelector("#condition");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
   cityTemperature.innerHTML = Math.round(response.data.temperature.current);
+  condition.innerHTML = response.data.condition.description;
+  humidity.innerHTML = response.data.temperature.humidity + " %";
+  wind.innerHTML = Math.round(response.data.wind.speed) + " km/h";
 }
 
 function changeCity(event) {
@@ -17,22 +23,24 @@ function changeCity(event) {
 let form = document.querySelector("#form");
 form.addEventListener("submit", changeCity);
 
-let currentTime = document.querySelector("#current-time");
-
-let daysOfTheWeek = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let myDate = new Date();
-let dayOfTheWeek = daysOfTheWeek[myDate.getDay()];
-currentTime.innerHTML =
-  dayOfTheWeek +
-  " " +
-  myDate.getHours() +
-  ":" +
-  String(myDate.getMinutes()).padStart(2, "0");
+function formatDate() {
+  let currentTime = document.querySelector("#current-time");
+  let daysOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let myDate = new Date();
+  let dayOfTheWeek = daysOfTheWeek[myDate.getDay()];
+  currentTime.innerHTML =
+    dayOfTheWeek +
+    " " +
+    myDate.getHours() +
+    ":" +
+    String(myDate.getMinutes()).padStart(2, "0");
+}
+formatDate();
